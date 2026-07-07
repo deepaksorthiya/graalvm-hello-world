b10671cdf94d7d52c1f54d707b033eb328701df6
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-07-07 12:11:43 +0530

    add assertj for fluent tests api

--- src/test/java/com/example/ReflectionTests.java
+++ src/test/java/com/example/ReflectionTests.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ReflectionTests {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionTests.class);
 
     @Test
     void testReflection() {
+        Class<?> foo = null;
         try {
-            Class<?> foo = Class.forName("com.example.Foo");
+            foo = Class.forName("com.example.Foo");
             LOGGER.info("Class: {}", foo);
         } catch (ClassNotFoundException e) {
             LOGGER.error("Error occurred while creating Foo instance", e);
         }
+        assertThat(foo).isNotNull();
     }
 }
--- src/test/java/com/example/SerializationDeserializationTests.java
+++ src/test/java/com/example/SerializationDeserializationTests.java
@@ -4,26 +4,33 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SerializationDeserializationTests {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SerializationDeserializationTests.class);
 
     @Test
     void testSerializationDeserialization() {
+        Person serializePerson = null;
+        Person deserializedPerson = null;
         try {
             // serialization and deserialization test
             // Create an instance of the Person class
-            Person person = new Person("John Doe", 30);
+            serializePerson = new Person("John Doe", 30);
 
             // Serialize the Person object to JSON
-            String jsonString = JsonUtil.serialize(person);
+            String jsonString = JsonUtil.serialize(serializePerson);
             LOGGER.info("Serialized JSON: {}", jsonString);
 
             // Deserialize the JSON back to a Person object
-            Person deserializedPerson = JsonUtil.deserialize(jsonString, Person.class);
+            deserializedPerson = JsonUtil.deserialize(jsonString, Person.class);
             LOGGER.info("Deserialized Person: {}, Age: {}", deserializedPerson.name(), deserializedPerson.age());
         } catch (Exception e) {
             LOGGER.error("Error occurred during serialization/deserialization", e);
         }
+        assertThat(serializePerson).isNotNull();
+        assertThat(deserializedPerson).isNotNull();
+
     }
 }
